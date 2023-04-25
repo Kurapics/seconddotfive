61d13ff8da6b8cd9423533134286a5d65e298a41
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:37:39 +0200

    Добавьте файлы проекта.

---
+++ seconddotfive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите символы: ");
+        string str = Console.ReadLine();
+        char[] mas = str.ToCharArray();
+        for (int i = 0; i < mas.Length; i++)
+            if (mas[i] >= '0' && mas[i] <= '9') Console.Write(mas[i]);
+        
+    }
+}
